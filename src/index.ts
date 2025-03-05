62ace5f9607c5e84f9f6a889193f0d7f1dc8c332
Abhishek Ritu <abhishekritu810219@gmail.com>
2025-03-06 00:22:32 +0530

    check

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello, TypeScript + Node.js Server is Running! 🚀");
 });
 
+
+app.get("/api", (req: Request, res: Response) => {
+    res.json({ message: "Hello, TypeScript + Node.js Server is Running! 🚀" });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
